fdbc6bcb1488d9191b7119bbe5d643c23ca4565c
Guthen <33220603+Guthen@users.noreply.github.com>
2024-01-22 11:33:01 +0100

    [update] v2.1.0 & base dependency to v2.2.0

--- lua/guthscp/modules/guthscp096/main.lua
+++ lua/guthscp/modules/guthscp096/main.lua
@@ -1,12 +1,12 @@
 local MODULE = {
 	name = "SCP-096",
 	author = "Guthen",
-	version = "2.0.1-beta",
+	version = "2.1.0",
 	description = [[Be SCP-096 and kill anyone who angered you while seeing your face, either by throwing doors or props at them or by dismembering them!]],
 	icon = "icon16/status_offline.png",
 	version_url = "https://raw.githubusercontent.com/Guthen/guthscp096/update-to-guthscpbase-remaster/lua/guthscp/modules/guthscp096/main.lua",
 	dependencies = {
-		base = "2.0.0",
+		base = "2.2.0",
 	},
 	requires = {
 		["shared.lua"] = guthscp.REALMS.SHARED,
